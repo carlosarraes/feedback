5ad8d101bd66a51976c75b40ce482afa5dc07a51
carraes <carraeshb@gmail.com>
2022-09-02 16:09:33 -0300

    Alguns componentes

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Card from './shared/Card';
+
+const FeedbackItem = ({ item }) => {
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+};
+
+export default FeedbackItem;
